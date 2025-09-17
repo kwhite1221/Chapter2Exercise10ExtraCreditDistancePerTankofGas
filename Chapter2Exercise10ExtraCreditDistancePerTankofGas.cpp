92c245f2b077a35bff3f61399c2ea7d0b51d24f6
Kristen White <kw21872@email.vccs.edu>
2025-09-17 14:13:00 -0400

    Added comments to match example

--- Chapter2Exercise10ExtraCreditDistancePerTankofGas.cpp
+++ Chapter2Exercise10ExtraCreditDistancePerTankofGas.cpp
@@ -1,19 +1,34 @@
+/* 
+Project Name: Chapter 2 Exercise 10 Distance Per Tank of Gas
+File Name: Chapter2Exercise10ExtraCreditDistancePerTankofGas.cpp
+Programmer: Kristen White
+Date 9/17/25
+Requirements: Calculate the number of miles per gallon the car gets.
+*/
 // This program calculates the miles per gallon a car gets. 
 
-# include <iostream>
-using namespace std;
+# include <iostream> // We will always need this library for writing
+// to the console and reading from the keyboard
+
+using namespace std; // We use this to simplify commands
 
 int main()
 {
-	double gallons = 15;
-	double miles = 375;
-	double mpg;
+	double gallons, miles, mpg;
+	
+	// Get the number of gallons from the user. 
+	cout << "Enter how many gallons the car holds: ";
+	cin >> gallons;
+
+	// Get the number of miles driven.
+	cout << "Enter how many miles were driven: ";
+	cin >> miles;
 
 	//Calculate the miles per gallon.
 	mpg = miles / gallons;
 
 	// Display the result.
 	cout << "The car gets " << mpg << " miles per gallon." << endl;
-
+	//It is best to have a return statement if the module is defined as returning a value.
 	return 0;
 }
